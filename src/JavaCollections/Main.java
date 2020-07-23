d4be3aeb055b3fbc2e5d89b12396343f10da53ec
mihai.straticiuc1995@gmai.com <myhai951>
2020-07-23 15:04:51 +0300

    Section 12

--- src/JavaCollections/Main.java
+++ src/JavaCollections/Main.java
@@ -3,8 +3,8 @@ package JavaCollections;
 public class Main {
 
     public static void main(String[] args){
-
-
+        System.out.println("New Section");
+        System.out.println("My name");
 
     }
 }
