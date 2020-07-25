cb12227dad4f84991bad98c2c328a2981b17aa91
mihai.straticiuc1995@gmai.com <myhai951>
2020-07-25 18:17:24 +0300

    Section 12

--- src/JavaCollections/Main.java
+++ src/JavaCollections/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args){
 
+        Theater theater = new Theater("My theatre", 8, 12);
+//        //theatre.getSeats();
+//        if (theater.reserveSeat("H11")){
+//            System.out.println("Please pay");
+//        }
+//        else {
+//            System.out.println("Seat is already taken");
+//        }
+
+
+
+
+
+
+
+
+
     }
 }
